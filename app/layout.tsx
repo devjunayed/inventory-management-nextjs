f47e841357ad5af4d4822b73ff04fe8a4b18db56
devjunayed <devjunayed@gmail.com>
2026-03-18 01:54:16 +0600

    stack-auth added and sign in page created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackClientApp } from "../stack/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <StackProvider app={stackClientApp}>
+          <StackTheme>{children}</StackTheme>
+        </StackProvider>
       </body>
     </html>
   );
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
+
+export default function SignInPage(){
+    return <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-300 to-purple-
+    100">
+        <div className="max-w-md w-full space-y-8">
+            <SignIn />
+            <Link href="/">Go Back Home</Link>
+        </div>
+    </div>
+}
